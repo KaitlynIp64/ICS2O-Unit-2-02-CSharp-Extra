2964ebdc89d5221647a23b123883b9dbe56b9914
KaitlynIp64 <99354960+KaitlynIp64@users.noreply.github.com>
2022-04-03 21:30:04 +0000

    fixed code

--- main.cs
+++ main.cs
@@ -1,7 +1,19 @@
-using System;
+// Created by: Kaitlyn Ip
+// Created on: Mar 2022
+//
+// This program does basic math
 
-class Program {
-  public static void Main (string[] args) {
-    Console.WriteLine ("Hello World");
-  }
+using System; 
+
+public class Program 
+{
+
+    public static void Main(string[] args) 
+    {
+        Console.WriteLine("The dimensions of a rectangle are: 5cm and 3cm.");
+        Console.WriteLine("The area is: " + (5 * 3) + "cm²");
+        Console.WriteLine("The perimeter is: " + (5 + 5 + 3 + 3) + "cm");
+
+        Console.WriteLine("\nDone.");
+    }
 }
