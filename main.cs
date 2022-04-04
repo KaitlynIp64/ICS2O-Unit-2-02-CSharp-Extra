b78a33e9e2d1558367d545fbe7271791f8d53579
KaitlynIp64 <99354960+KaitlynIp64@users.noreply.github.com>
2022-04-04 01:18:56 +0000

    fixed whitespace

--- main.cs
+++ main.cs
@@ -3,12 +3,12 @@
 //
 // This program does basic math
 
-using System; 
+using System;
 
-public class Program 
+public class Program
 {
 
-    public static void Main(string[] args) 
+    public static void Main(string[] args)
     {
         Console.WriteLine("The dimensions of a rectangle are: 5cm and 3cm.");
         Console.WriteLine("The area is: " + (5 * 3) + "cm²");
